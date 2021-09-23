079dcbbef6b4b16d0fb53eb65c2fdab03343bf35
Verneri <44767@edu.raseko.fi>
2021-09-23 10:07:56 +0300

    Tietokantamuutoksia

---
+++ testData/asiakas_202109161010.sql
@@ -0,0 +1,5 @@
+INSERT INTO asiakas (asiakasnumero,yritys,laskutusosoite,postinumero,postitoimipaikka) VALUES
+	 (1,'Raseko','Purokatu 1','21200','Raisio'),
+	 (2,'Mähönen Oy','Kuormakatu 2','21200','Raisio'),
+	 (3,'Autoliike Petos & Vilppi Ky','Keskuskatu 22','23100','Mynämäki'),
+	 (4,'Huttu ja Keitto Ay','Puoskarinkatu 2 A 11','20100','Turku');
